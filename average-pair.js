fec0aaf1aeab0b22bc88f14f8f1ffed09be7b97b
Maurice <maurice.baez1@gmail.com>
2022-03-11 16:20:08 -0800

    completed average-pair

--- average-pair.js
+++ average-pair.js
@@ -1,3 +1,24 @@
-// add whatever parameters you deem necessary & write docstring
-function averagePair() {
+"use strict";
+
+
+
+/** function will accept two parameters. First parameter is a sorted array, the second parameter is a number that is our target average.
+ * We'll use multiple pointers to move through the front and back of the array determining if the paired values equals the target average.
+ */
+function averagePair(nums, targetAverage) {
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+
+    if ((nums[left] + nums[right] / 2) === targetAverage) {
+      return true;
+    }
+    else if ((nums[left] + nums[right] / 2) > targetAverage) {
+      right--;
+    }
+    else left++;
+  }
+  return false;
 }
